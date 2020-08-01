e8d8e34ff1c9162d49c138c4da8926f0d570de26
Duncan Haywood <duncanhaywoodacademic@gmail.com>
2020-08-01 07:15:10 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const bodyParser = require('body-parser');
+const path = require(`path`);
+const express = require('express');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/submit', (req, res) => {
+  console.log({
+    name: req.body.name,
+    message: req.body.message
+  });
+  res.send('Thanks for your message!');
+});
+
+app.get('/submit', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/form.html'));
+});
+
+app.get('/', (req, res) => {
+  res.send('Hi, Thank you for visiting Duncans first node.js app. I hope you have a spectacular day!');
+});
+
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
